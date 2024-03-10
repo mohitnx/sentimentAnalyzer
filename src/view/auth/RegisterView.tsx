fefa70ccebb9de45bfed394c33deae546541a1a8
mohitnx <mohitneupane157@gmail.com>
2024-03-10 15:27:54 +0545

    changed fileds

--- src/view/auth/RegisterView.tsx
+++ src/view/auth/RegisterView.tsx
@@ -3,6 +3,9 @@ import { Link } from "react-router-dom";
 import * as yup from "yup";
 
 const validationSchema = yup.object({
+  firstName: yup.string().required("First Name is required"),
+  lastName: yup.string().required("Last Name is required"),
+  username: yup.string().required("Username is required"),
   email: yup
     .string()
     .email("Invalid email format")
@@ -11,13 +14,21 @@ const validationSchema = yup.object({
     .string()
     .min(5, "Password must be at least 5 characters")
     .required("Password is required"),
+  confirmPassword: yup
+    .string()
+    .oneOf([yup.ref("password")], "Passwords must match")
+    .required("Please confirm your password"),
 });
 
 const RegisterView = () => {
   const formik = useFormik({
     initialValues: {
+      firstName: "",
+      lastName: "",
+      username: "",
       email: "",
       password: "",
+      confirmPassword: "",
     },
     validationSchema: validationSchema,
     onSubmit: (values, helpers) => {
@@ -36,7 +47,7 @@ const RegisterView = () => {
     <form onSubmit={formik.handleSubmit}>
       <div className="bg-lightBlack w-full h-[100vh]">
         <div className="absolute translate-x-[-50%] translate-y-[-50%]  top-[50%] left-[50%] ">
-          <div className="bg-black w-[585px] h-[757px] rounded-[4px] pt-[85px] pb-[55px] px-[80px] flex flex-col justify-between ">
+          <div className="bg-black w-[585px] h-[full] rounded-[4px] pt-[85px] pb-[55px] px-[80px] flex flex-col justify-between ">
             <div className="flex flex-col border-b-[1px] border-b-[rgba(0,0,0,0.10)]">
               <p className="font-[700] text-[30px] text-white select-none text-center">
                 Greetings
@@ -44,6 +55,45 @@ const RegisterView = () => {
               <p className="font-[400] text-[18px] text-[#dedbdb] pt-[5px] pb-[36px] select-none text-center">
                 Please create an account to continue
               </p>
+              <div className="w-[100%] pb-[24px]">
+                <input
+                  title=""
+                  placeholder="firstName"
+                  className="mt-1 p-2 w-full border rounded-md bg-gray-800 text-gray-200 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  {...formik.getFieldProps("firstName")}
+                />
+                {formik.touched.firstName && formik.errors.firstName ? (
+                  <div className="text-red-500 text-[14px] px-2">
+                    {formik.errors.firstName}
+                  </div>
+                ) : null}
+              </div>
+              <div className="w-[100%] pb-[24px]">
+                <input
+                  title=""
+                  placeholder="lastName"
+                  className="mt-1 p-2 w-full border rounded-md bg-gray-800 text-gray-200 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  {...formik.getFieldProps("lastName")}
+                />
+                {formik.touched.lastName && formik.errors.lastName ? (
+                  <div className="text-red-500 text-[14px] px-2">
+                    {formik.errors.lastName}
+                  </div>
+                ) : null}
+              </div>
+              <div className="w-[100%] pb-[24px]">
+                <input
+                  title=""
+                  placeholder="Username"
+                  className="mt-1 p-2 w-full border rounded-md bg-gray-800 text-gray-200 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  {...formik.getFieldProps("username")}
+                />
+                {formik.touched.username && formik.errors.username ? (
+                  <div className="text-red-500 text-[14px] px-2">
+                    {formik.errors.username}
+                  </div>
+                ) : null}
+              </div>
               <div className="w-[100%] pb-[24px]">
                 <input
                   title=""
@@ -68,6 +118,21 @@ const RegisterView = () => {
                 {formik.touched.password && formik.errors.password ? (
                   <div className="text-red-500 text-[14px] px-2">
                     {formik.errors.password}
+                  </div>
+                ) : null}
+              </div>
+              <div className="w-[100%] pb-[24px]">
+                <input
+                  type="password"
+                  title=""
+                  placeholder="confirm password"
+                  className="mt-1 p-2 w-full border rounded-md bg-gray-800 text-gray-200 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  {...formik.getFieldProps("confirmPassword")}
+                />
+                {formik.touched.confirmPassword &&
+                formik.errors.confirmPassword ? (
+                  <div className="text-red-500 text-[14px] px-2">
+                    {formik.errors.confirmPassword}
                   </div>
                 ) : null}
               </div>
